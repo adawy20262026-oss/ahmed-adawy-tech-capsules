e14b3cf54405334cf041183bc3be8e17fafd2213
adawy20262026-oss <adawy20262026@gmail.com>
2026-07-29 21:49:18 +0300

    Add project configuration

---
+++ src/config.py
@@ -0,0 +1,18 @@
+"""
+Project Configuration
+Ahmed Adawy Tech Capsules
+"""
+
+PROJECT_NAME = "Ahmed Adawy Tech Capsules"
+
+AUTHOR = "Ahmed Adawy"
+
+VERSION = "0.1.0"
+
+CAPSULES_DIR = "capsules"
+
+OUTPUT_DIR = "output"
+
+TEMPLATES_DIR = "templates"
+
+ASSETS_DIR = "assets"
