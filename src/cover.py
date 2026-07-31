f402e50cbc73c1205162d18d4c3f6a8c917fff92
adawy20262026-oss <adawy20262026@gmail.com>
2026-07-31 17:19:26 +0300

    Extract cover into cover.py

--- src/cover.py
+++ src/cover.py
@@ -1,1 +1,55 @@
+"""
+Cover Renderer
+Ahmed Adawy Tech Capsules
+"""
 
+
+class CoverRenderer:
+
+    def render(self, metadata):
+
+        title = metadata.get(
+            "title",
+            "Ahmed Adawy Tech Capsules"
+        )
+
+        subtitle = metadata.get(
+            "subtitle",
+            ""
+        )
+
+        author = metadata.get(
+            "author",
+            "Ahmed Adawy"
+        )
+
+        version = metadata.get(
+            "version",
+            "1.0"
+        )
+
+        return f"""
+<div class="cover">
+
+<p>Ahmed Adawy Tech Capsules</p>
+
+<h1>{title}</h1>
+
+<h2>{subtitle}</h2>
+
+<p>
+
+<b>Author</b><br>
+
+{author}
+
+</p>
+
+<p>
+
+Version {version}
+
+</p>
+
+</div>
+"""
