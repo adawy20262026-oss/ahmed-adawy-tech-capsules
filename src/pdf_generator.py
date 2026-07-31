84c2a18e953fc174935017618769424781f9a366
adawy20262026-oss <adawy20262026@gmail.com>
2026-08-01 01:21:34 +0300

    Update pdf_generator.py

--- src/pdf_generator.py
+++ src/pdf_generator.py
@@ -1,16 +1,23 @@
-"""
-PDF Generator
-Ahmed Adawy Tech Capsules
-"""
-
+from io import BytesIO
 from pathlib import Path
 from weasyprint import HTML
 
 
 class PDFGenerator:
 
-    def generate(self, html: str, output_file: Path):
+    def generate(self, html: str, output_file: Path | None = None):
+        """
+        إذا تم تمرير output_file:
+            يحفظ الملف ويرجع المسار.
+
+        إذا لم يتم تمرير output_file:
+            يرجع PDF Bytes (لاستخدام Streamlit).
+        """
+
+        if output_file is None:
+            pdf = BytesIO()
+            HTML(string=html).write_pdf(pdf)
+            return pdf.getvalue()
 
         HTML(string=html).write_pdf(output_file)
-
         return output_file
