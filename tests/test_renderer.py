ea66c25d385759941b2315c7a2088aa40c533286
adawy20262026-oss <adawy20262026@gmail.com>
2026-08-03 16:31:40 +0300

    Update test_renderer.py

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -1,1 +1,31 @@
+from renderer import HTMLRenderer
 
+
+def test_renderer_returns_html():
+
+    renderer = HTMLRenderer()
+
+    html = renderer.render([])
+
+    assert "<html" in html
+    assert "</html>" in html
+
+
+def test_renderer_contains_body():
+
+    renderer = HTMLRenderer()
+
+    html = renderer.render([])
+
+    assert "<body>" in html
+    assert "</body>" in html
+
+
+def test_renderer_contains_title():
+
+    renderer = HTMLRenderer()
+
+    html = renderer.render([])
+
+    assert "<title>" in html
+    assert "Tech Capsule" in html
