1cfb2f3c57e839be74e862bdac4963a811860f94
adawy20262026-oss <adawy20262026@gmail.com>
2026-08-03 13:56:38 +0300

    Update test_builder.py

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,1 +1,20 @@
 
+from builder import CapsuleBuilder
+
+
+def test_builder_creation():
+    builder = CapsuleBuilder()
+
+    assert builder is not None
+
+
+def test_builder_has_build():
+    builder = CapsuleBuilder()
+
+    assert hasattr(builder, "build")
+
+
+def test_builder_has_build_all():
+    builder = CapsuleBuilder()
+
+    assert hasattr(builder, "build_all")
