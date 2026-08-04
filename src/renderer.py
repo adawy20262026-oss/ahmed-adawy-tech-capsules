17291d57aa2fb9915d32f72efde78fd5f70dd4ee
adawy20262026-oss <adawy20262026@gmail.com>
2026-08-04 22:47:22 +0300

    Update renderer.py

--- src/renderer.py
+++ src/renderer.py
@@ -3,11 +3,11 @@ HTML Renderer
 Ahmed Adawy Tech Capsules
 """
 
-from styles import get_styles
-from cover import CoverRenderer
-from toc import TOCRenderer
-from content_renderer import ContentRenderer
-from footer import FooterRenderer
+from .styles import get_styles
+from .cover import CoverRenderer
+from .toc import TOCRenderer
+from .content_renderer import ContentRenderer
+from .footer import FooterRenderer
 
 
 class HTMLRenderer:
