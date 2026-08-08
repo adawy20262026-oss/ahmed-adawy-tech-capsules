30a188ee931b4e45ab91ce9ed9531c2f14ad33b8
adawy20262026-oss <adawy20262026@gmail.com>
2026-08-08 12:09:15 +0300

    Update highlighter.py

--- src/highlighter.py
+++ src/highlighter.py
@@ -18,27 +18,40 @@ class SyntaxHighlighter:
 
         self.formatter = HtmlFormatter(
             nowrap=False,
-            cssclass="highlight"
+            cssclass="highlight",
+            style="monokai",
         )
 
-    def highlight(self, code: str, language: str = "") -> str:
+    def highlight(
+        self,
+        code: str,
+        language: str = "",
+    ) -> str:
 
         if not language:
+
             lexer = TextLexer()
 
         else:
+
             try:
-                lexer = get_lexer_by_name(language)
+
+                lexer = get_lexer_by_name(
+                    language
+                )
 
             except ClassNotFound:
+
                 lexer = TextLexer()
 
         return highlight(
             code,
             lexer,
-            self.formatter
+            self.formatter,
         )
 
     def css(self):
 
-        return self.formatter.get_style_defs(".highlight")
+        return self.formatter.get_style_defs(
+            ".highlight"
+        )
